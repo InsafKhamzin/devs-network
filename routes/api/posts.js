4c285f08cb9e926d46300121a92b051fde93ca3d
Insaf <hamzin730insaf@gmail.com>
2020-03-07 22:47:16 -0500

    posts add/delete comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -95,7 +95,7 @@ router.delete('/:id', auth, async (req, res) => {
 
 // @route PUT api/posts/like/:id
 // @desc Like/Unlike post
-router.put('/like/:id', auth, async (req, res) => {
+router.put('/:id/like', auth, async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
         if (!post) {
@@ -119,7 +119,81 @@ router.put('/like/:id', auth, async (req, res) => {
     }
 });
 
+// @route POST api/posts/comment/:id
+// @desc Comment on a post
+router.post('/:id/comment',
+    [
+        auth,
+        [
+            check('text', 'Text is required').notEmpty()
+        ]
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
 
+            if (!ObjectId.isValid(req.params.id)) {
+                return res.status(400).json({ msg: 'Invalid post id' })
+            }
+            const post = await Post.findById(req.params.id);
+            if (!post) {
+                return res.status(400).json({ msg: 'Post not found' });
+            }
 
+            const user = await User.findById(req.user.id);
+
+            const newComment = {
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            };
+
+            post.comments.unshift(newComment);
+
+            await post.save();
+            res.json(post.comments);
+        } catch (error) {
+            console.error(error.message);
+            res.send('Server Error');
+        }
+    });
+
+// @route DELETE api/posts/comment/:id
+// @desc Delete comment
+router.delete('/:id/comment/:comment_id', auth, async (req, res) => {
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            return res.status(400).json({ msg: 'Invalid post id' })
+        }
+        if (!ObjectId.isValid(req.params.comment_id)) {
+            return res.status(400).json({ msg: 'Invalid comment id' })
+        }
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        const comment = post.comments
+            .find(comment => comment._id.toString() === req.params.comment_id);
+        if(!comment){
+            return res.status(404).json({ msg: 'Comment not found' });
+        }
+
+        if(comment.user.toString() !== req.user.id){
+            return res.status(401).json({ msg: 'Not authorized' });
+        }
+
+        await post.comments.remove(comment._id);
+        await post.save();
+        res.json(post.comments);
+    } catch (error) {
+        console.error(error.message);
+        res.send('Server Error');
+    }
+});
 
 module.exports = router;
